338b0766e062f2bdbd49c2540086a4ae43bdcc3f
otavio <barbosaotavio293@gmail.com>
2025-08-09 22:24:42 -0300

    variaveis

--- variaveis.c
+++ variaveis.c
@@ -2,9 +2,16 @@
 
 int manin(){
 
-    int idade = 29;
-    int quantidaed = 1;
+    int idade;
+    int quantidade = 1;
     float altura = 1.73;
     char letra = 'A';
     char nome[20] = 'Otávio'; 
+
+    // Decladondo um valor a variável idade.
+    idade = 29;
+
+    // Substituindo o valor da variável quantidade.
+    quantidade = 10;
+
 }
