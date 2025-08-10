95a4fe5c33fd32a199e8d28a9cfe3eab3dc6fef2
otavio <barbosaotavio293@gmail.com>
2025-08-09 23:26:11 -0300

    testes

---
+++ OlaMundo/variaveis.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int manin(){
+
+    int idade = 30;
+    int quantidade = 1;
+    float altura = 1.73;
+    char nome[30] = "Otavio";
+
+    
+
+printf("A idade do %s é: %d\n", nome, idade);
+    
+}
+
+ 
