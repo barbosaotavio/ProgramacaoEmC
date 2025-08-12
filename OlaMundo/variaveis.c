4796d72d9332161b8512c8d57cbbdfd35eeed2f9
otavio <barbosaotavio293@gmail.com>
2025-08-11 23:36:55 -0300

    variaveis

--- OlaMundo/variaveis.c
+++ OlaMundo/variaveis.c
@@ -5,10 +5,26 @@ int main(){
     int idade = 55;
     int quantidade = 1;
     float altura = 1.73;
+    char opcao = 'O';
     char nome[30] = "Otavio";
 
     
 
-    printf("A idade do %s é: %d\n", nome, idade);
+printf("A idade do %s é: %d\n", nome, idade);
+printf("A altura é: %.2f\n", altura);
+printf("A opção é: %c\n", opcao);
     
+
+    /*
+    printf("%formato1 %formato2 %formato3", varial1,varialvel2,variavel3)
+
+    %d: Imprime um número inteiro no formato decimal.
+    %i: Equivale a %d.
+    %f: Imprime um número de ponto flutuante no formato padrão.
+    %e: Imprime um número de ponto flutuante na notação científica.
+    %c: Imprime um unico caractere.
+    %s: Imprime uma cadeia de caracteres(string).
+
+    */
 }
+
