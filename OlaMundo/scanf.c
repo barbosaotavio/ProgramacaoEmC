14c5daa50e835f5cf1e0d45dc18658d3373b73db
otavio <barbosaotavio293@gmail.com>
2025-08-11 23:58:07 -0300

    scanf

---
+++ OlaMundo/scanf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    int idade;
+    float altura;
+    char opcao;
+    char nome[20];
+    
+    printf("Digite a sua idade:");
+    scanf("%d", &idade);
+    printf("A idade é: %d\n", idade);
+
+    printf("Digite a sua altura:");
+    scanf("%f", &altura);
+    printf("A sua altura é:%f\n", altura);
+
+}
