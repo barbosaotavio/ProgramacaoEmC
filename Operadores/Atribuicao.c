95b8be8e82533adf4e3779c9aba89128bbcbf4ef
otavio <barbosaotavio293@gmail.com>
2025-08-16 21:36:48 -0300

    Atribuicao

---
+++ Operadores/Atribuicao.c
@@ -0,0 +1,22 @@
+#include <stdio.h> 
+
+int main(){
+
+    int numero1, numero2, resultado;
+
+    resultado = 10;
+    printf("resultado %d\n", resultado);
+
+    resultado += 50;
+    printf("resultado %d\n", resultado);
+
+    resultado -= 5;
+    printf("resultado %d\n", resultado);
+
+    resultado *= 10;
+    printf("resultado %d\n", resultado);
+
+    resultado /= 5;
+    printf("resultado %d\n", resultado);
+
+}
