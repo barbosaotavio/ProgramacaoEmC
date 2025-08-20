e302dd4c1cb2ae311c57cf680e4d5110be847080
otavio <barbosaotavio293@gmail.com>
2025-08-20 03:05:30 -0300

    float

--- Operadores/Conversao.c
+++ Operadores/Conversao.c
@@ -14,4 +14,16 @@ int main(){
 
     printf("Quociente: %.2f\n", quociente);
 
+    int x = 15;
+    int y = 2;
+    float res = (float) x / y;
+
+    printf("Res é: %.2f\n", res);
+
+    int m = 19;
+    int n = 2;
+    float resultado = (float) m / n;
+
+    printf("O resultado é: %.1f\n", resultado);
+
 }
