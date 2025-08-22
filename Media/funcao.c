6b83c55e0d254d55f5a9dfb51872142ed461bafd
otavio <barbosaotavio293@gmail.com>
2025-08-21 23:05:31 -0300

    testes

---
+++ Media/funcao.c
@@ -0,0 +1,26 @@
+# include <stdio.h>
+
+    // Função genérica para entrada de dados.
+void entradaDados(){
+    // Código para a função entradaDados. 
+    // Entrada de dados(scanf)
+    }
+
+
+    // Função genérica para cálculo de média.
+float calcularMedia(){
+    // Código para a função calcularMedia.
+}    
+
+
+    // Função genérica para exibir dados.
+void exibirMedia(){
+    //  Código para a função exibirMedia.
+}
+
+int main(){
+    entradaDados();
+    float media = calcularMedia();
+    exibirMedia();
+    return 0;
+}
