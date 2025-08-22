47750bacc08320008aa932ac793bf634f549066b
otavio <barbosaotavio293@gmail.com>
2025-08-21 23:35:40 -0300

    calcularMedia

--- Media/CalcularMedia.c
+++ Media/CalcularMedia.c
@@ -1,9 +1,53 @@
 #include <stdio.h>
 
-int main(){
+ int main(){
 
-    int nota1, nota2, nota3;
-    int media;
+//     int nota1, nota2, nota3;
+//     int media;
+    
+
+//     printf("*** Programa de Cálculo de Média ***\n");
+
+//     printf("Primeira nota\n");
+//     scanf("%d", &nota1);
+    
+//     printf("Segunda nota\n");
+//     scanf("%d", &nota2);
+
+//     printf("Terceira nota\n");
+//     scanf("%d", &nota3);
+
+//     media = (nota1 + nota2 + nota3) / 3;
+
+//     printf("A média do aluno é: %d\n", media);
+
+
+
+    // Usando o float
+
+     float nota1, nota2, nota3;
+     float media;
+    
+
+    printf("*** Programa de Cálculo de Média ***\n");
+
+    printf("Primeira nota\n");
+     scanf("%f", &nota1);
+    
+    printf("Segunda nota\n");
+     scanf("%f", &nota2);
+
+    printf("Terceira nota\n");
+     scanf("%f", &nota3);
+
+    media = (nota1 + nota2 + nota3) / 3;
+
+    printf("A média do aluno é: %.1f\n", media);
+
+    // Usando o Cast (conversão explicita)
+
+    /*int nota1, nota2, nota3;
+    float media;
     
 
     printf("*** Programa de Cálculo de Média ***\n");
@@ -17,8 +61,9 @@ int main(){
     printf("Terceira nota\n");
     scanf("%d", &nota3);
 
-    media = (nota1 + nota2 + nota3) / 3;
+    media = (float) (nota1 + nota2 + nota3) / 3;
 
-    printf("A média do aluno é: %d\n", media);
+    printf("A média do aluno é: %.1f\n", media);
+    */
 
 }
