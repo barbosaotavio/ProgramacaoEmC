fa48f44a60782e25533bbc6cb37dd084e1ae08a6
otavio <barbosaotavio293@gmail.com>
2025-08-23 22:15:15 -0300

    modificadores

---
+++ Modificadores/sizeof.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("Tamanho de int");
+    printf("Tamanho de long int");
+    printf("Tamanho de long long int");
+    printf("Tamanho de double");
+    printf("Tamanho de long double");
+    
+    return 0;
+}
