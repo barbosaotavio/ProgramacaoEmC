c1b5d7d8be4fa6560a9a1eec133ec4b98e8f04c4
otavio <barbosaotavio293@gmail.com>
2025-08-25 23:29:57 -0300

    Operadores relacionais

---
+++ OperadoresRelacionais/Comparacoes.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    
+    int x = 5;
+    float y = 5.0;
+    char c = 'a';
+
+    printf("x >= y %d\n", x >= y);
+    printf("x == y %d\n", x == y);
+    printf("x != y %d\n", x != y);
+
+
+    printf("x >= c %d \n", x >= c);
+    printf(" O valor de ASCII de %c é %d\n", c, c);
+} 
---
+++ OperadoresRelacionais/Operadores.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+
+    /*
+    Operadores Relacionais
+
+    > (maior que)
+    < (menor que)
+    >= (maior ou igual a)
+    <= (menor ou igual a)
+    == (igual a)
+    != (diferente de)
+
+    1 para verdadeiro
+    0 para falso
+    */
+
+    int a = 10;
+    int b = 20;
+
+    printf("a > b: %d \n", a > b);
+    printf("a < b: %d \n", a < b);
+    printf("a == b: %d \n", a == b);
+    printf("a != b: %d \n", a != b);
+    
+
+}
