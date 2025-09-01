72096fc92e36264a583785979177b7271f129f6d
otavio <barbosaotavio293@gmail.com>
2025-08-31 22:03:34 -0300

    ifs

---
+++ Condicionais.c/If_2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+    // int numero = 10;
+
+    // if (numero % 2 == 0){
+    //     printf("O número é par!");
+    // }
+
+    int numero = 10, resultado;
+    
+    resultado = numero % 2;
+    printf("A variável resultado é: %d\n", resultado);
+
+    if( resultado == 0){
+        printf("O número é par!");
+    }
+
+
+
+}
