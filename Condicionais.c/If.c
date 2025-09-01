612a5023a18274b9581cf75d99fdd05fc5c98ac0
otavio <barbosaotavio293@gmail.com>
2025-08-31 21:25:37 -0300

    if

---
+++ Condicionais.c/If.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+
+//     int numero1 = 2;
+//     int numero2 = 5;
+
+// if (numero1 > numero2) {
+//     printf("numero1 é maior que numero2\n");
+// }
+
+// return 0;
+
+    int idade = 16;
+
+     if(idade >= 18){
+
+        
+        printf("Você é maior de idade!\n");
+        printf("Dentro if\n");
+     }
+
+        printf("Fora if");
+
+
+
+}
