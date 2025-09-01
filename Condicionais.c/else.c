484277b000c5521c098e9931a2243e4867e97030
otavio <barbosaotavio293@gmail.com>
2025-08-31 22:31:36 -0300

    if else

---
+++ Condicionais.c/else.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main(){
+
+    // float temperatura1 = 30.5;
+    // float temperatura2 = 15.1;
+
+    // if(temperatura1 < temperatura2){
+    //     printf("Está calor!");
+    // } else printf("Está frio!");
+
+
+        // -------------------------- //
+
+    // int banana = 75;
+    // int laranja = 81;
+
+    // if(banana < laranja){
+    //     printf("Tem mais banana!");
+    // } else printf("Tem mais laranja!");
+
+        // -------------------------- //
+
+    // int numero = 37;
+    
+    // if(numero % 2 == 0){
+    //     printf("O número é PAR!");
+    // }else printf("O número é ÍMPAR");
+
+        //--------------------------- //
+
+    int numero = 30, resultado;
+
+    resultado = numero % 2;
+        printf("O valor da variável resultado é: %d\n", resultado);
+
+    if(resultado == 0){
+        printf("O número é PAR!");
+    } else printf("O número é ÍMPAR!");  
+
+}
