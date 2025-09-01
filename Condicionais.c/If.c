63b1673fe2e3433586fb94c7e6f6f8f7e6cdf634
otavio <barbosaotavio293@gmail.com>
2025-08-31 21:50:44 -0300

    testes IF

--- Condicionais.c/If.c
+++ Condicionais.c/If.c
@@ -11,17 +11,72 @@ int main(){
 
 // return 0;
 
-    int idade = 16;
+    // int idade = 16;
 
-     if(idade >= 18){
+    //  if(idade >= 18){
 
         
-        printf("Você é maior de idade!\n");
-        printf("Dentro if\n");
-     }
+    //     printf("Você é maior de idade!\n");
+    //     printf("Dentro if\n");
+    //  }
 
-        printf("Fora if");
+    //     printf("Fora if");
 
 
 
+    
+
+    // float temperatura = 32.0;
+
+    // if (temperatura > 30.0){
+    //     printf("Está calor\n");
+    // }
+
+    
+
+
+    // float media = 70.0;
+
+    // if (media >= 60.0){
+    //     printf("Parabéns, você passou!");
+    // }
+    
+
+
+    
+    // int idade1 = 25;
+    // int idade2 = 30;
+
+    // if (idade1 > idade2){
+    //     printf("Pessoa 1 é mais velha que Pessoa 2\n");
+    // }   
+
+
+
+
+    // int idade1 = 25;
+    // int idade2 = 30;
+
+    // if (idade1 < idade2){
+    //     printf("Pessoa 1 é mais nova que Pessoa 2!\n");
+    // }   
+
+
+    //  int idade1 = 30;
+    //  int idade2 = 30;
+
+    //  if (idade1 == idade2){
+    //      printf("As Pessoas tem mesma idade!\n");
+    //  }   
+
+    int idade1 = 25;
+    int idade2 = 30;
+
+    if (idade1 != idade2){
+        printf("As pessoas tem idade diferente!\n");
+    }   
+
+
+
+        
 }
