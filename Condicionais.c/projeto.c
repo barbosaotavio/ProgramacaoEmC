7fed1e986ad178001b969f0ea120226ac5544347
otavio <barbosaotavio293@gmail.com>
2025-09-03 20:36:36 -0300

    testes if else

---
+++ Condicionais.c/projeto.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+
+    float temperatura, umidade;
+    unsigned int estoque, estoqueMinimo = 1000;
+     
+
+    printf("Entre com a temperatura:\n");
+    scanf("%f", &temperatura);
+
+    printf("Entre com a umidade:\n");
+    scanf("%f", &umidade);
+
+    printf("Entre com o estoque:\n");
+    scanf("%u", &estoque);
+
+    if(temperatura > 30){
+        printf("A temperadura esta alta.\n");
+    } else{
+        printf("A temperatura está dentro dos parâmetros\n");
+    }
+
+    if(umidade > 50){
+        printf("Umidade elevada.\n");
+    } else{
+        printf("A temperatura está dentro dos parâmetros\n");
+    }
+
+    if(estoque < estoqueMinimo){
+        printf("Estoque abaixo do mínimo.\n");
+    } else{
+        printf("Estoque normal.\n");
+    }
+}
