ce065969a5c41282c284f418cacada994d95a111
otavio <barbosaotavio293@gmail.com>
2025-09-09 01:38:42 -0300

    aninhadas

---
+++ EstruturasDeDescisao/Aninhadas.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+
+    int idade, renda;
+
+    printf("Digite a sua idade:\n");
+    scanf("%d", &idade);
+
+    printf("Digite a sua renda:\n");
+    scanf("%d", &renda);
+
+    if(idade <= 18 || idade >= 60){
+        if(renda > 2000){
+            printf("você tem direito ao desconto!\n");
+        } else{
+            printf("Você não tem direito ao desconto devido a renda!\n");
+        }
+        
+    }else{
+        printf("Você não atende aos critérios devido a idade!\n");
+    }
+
+}
