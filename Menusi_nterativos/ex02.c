99755a5f8e001ae0509f08bfb22afff1b8ec2c7b
otavio <barbosaotavio293@gmail.com>
2025-09-14 00:21:48 -0300

    numero secreto

--- Menusi_nterativos/ex02.c
+++ Menusi_nterativos/ex02.c
@@ -4,7 +4,7 @@
 
 int main(){
 
-    int opcao;
+    int opcao, regras;
     int numeroSecreto, palpite;
 
     printf("Menu Principal\n");
@@ -13,5 +13,50 @@ int main(){
     printf("3. Sair\n");
     printf("Escolha uma opção:\n");
     scanf("%d", &opcao);
+
+    switch (opcao)
+    {
+    case 1:
+        srand(time(0));
+        numeroSecreto = rand() % 10;
+        printf("Digite um número de 0 a 9: ");
+        scanf("%d", &palpite);
+        if (numeroSecreto == palpite)
+        {
+            printf("Você acertou!\n");
+            printf("Número secreto %d\n", numeroSecreto);
+        }else{
+            printf("Você errou!\n");
+            printf("Número secreto %d\n", numeroSecreto);
+        }
+        
+        break;
+
+    case 2:
+        printf("Explicação das regras!\n");
+        printf("Digite a opção relacionada as regras do jogo!\n");
+        scanf("%d", &regras);
+
+        switch (regras)
+        {
+        case 1:
+            printf("O jogo é bem simples, você precisa adivinhar o número secreto que o jogo gera!\n");
+            break;
+
+        case 2:
+            printf("Digite um número de 0 a 9 e veja se você acertou!\n");
+            break;
+        
+        default:
+        }
+
+        break;
+
+    case 3:
+        printf("Saindo do jogo!\n");
+        break;
+    default:
+        printf("Opção Invalida!\n");
+    }
 
 }
