7953d30145918d9e238d343cdc115d1a0e41479a
otavio <barbosaotavio293@gmail.com>
2025-09-13 23:53:33 -0300

    Menu de jogo

---
+++ Menusi_nterativos/ex02.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+
+    int opcao;
+    int numeroSecreto, palpite;
+
+    printf("Menu Principal\n");
+    printf("1. Iniciar Jogo\n");
+    printf("2. Ver Regras\n");
+    printf("3. Sair\n");
+    printf("Escolha uma opção:\n");
+    scanf("%d", &opcao);
+
+}
