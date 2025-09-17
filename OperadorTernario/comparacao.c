122c793ab5c6c3d2ca4920c0a5ae7cd9c3ca6b17
otavio <barbosaotavio293@gmail.com>
2025-09-17 17:26:22 -0300

    ternario

---
+++ OperadorTernario/comparacao.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    int num1 = 40, num2 = 59;
+    int maior;
+
+    if (num1 > num2){
+        printf("O número maior é: %d\n",num1);
+    }else{
+        printf("O número maior.é: %d\n",num2);
+    }
+  
+    
+
+    // num1 > num2 ? (maior = num1) : (maior = num2);
+
+    // printf("O número maior é: %d", maior);
+
+}
