dc55e28250d155fad62b007f9aee6b3363faf51f
otavio <barbosaotavio293@gmail.com>
2025-09-22 23:40:57 -0300

    exFor

---
+++ For/ex02.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+        //Inicialização ; Condição; Incremento/decremento
+    for(int i = 0; i <= 100; i += 10){
+        
+        printf("%d\n", i);
+    }
+
+}
