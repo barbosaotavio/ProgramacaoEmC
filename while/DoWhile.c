ddd7a5baf7249e3925b0d37bb1de4f05afd3025c
otavio <barbosaotavio293@gmail.com>
2025-09-22 23:20:58 -0300

    do while

---
+++ while/DoWhile.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    int i = 1;  // Variável de incremento.
+
+    do{ //Entrada 1x pelo menos     
+        printf("%d\n", i); // Saída
+
+        i++; // Incremento
+
+    } while ( i <= 5); // Condição
+}
+    
