a8f14772389f9d9a8576d22120c1879429b7f15b
otavio <barbosaotavio293@gmail.com>
2025-09-22 23:44:48 -0300

    ex03

---
+++ For/ex03.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+    //Iniciando a variáel com o valor 100 e fazendo o decremento de -10.
+    for(int i = 100; i >= 10; i -=10){
+        printf("%d\n", i);
+    }
+
+}
