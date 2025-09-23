80d864ba1e7fcd3b93a505a248e7ffc4c6395bd3
otavio <barbosaotavio293@gmail.com>
2025-09-23 00:39:19 -0300

    loops

---
+++ Loops/ex01.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+
+    //  int i = 0;
+
+    //  while (i <= 10){
+        
+    //      if (i % 2 == 0 ){
+    //          printf("O número %d é par!\n", i);
+    //      }
+        
+    //      i++;
+    //  }
+
+    int i = 0;
+
+    while (i <= 10){
+        
+        if (i % 2 != 0 ){
+            printf("O número %d é Impar!\n", i);
+        }
+        
+        i++;
+    }
+    
+    
+
+}
