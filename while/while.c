c3e26e0d44ef6da876891716cd06e47ab632bdf1
otavio <barbosaotavio293@gmail.com>
2025-09-22 22:55:03 -0300

    while

---
+++ while/while.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    int i = 1;  // Variável de incremento.
+
+    while ( i <= 5) // Condição
+    {
+        printf("%d\n", i); // Saída
+
+        i++; // Incremento
+
+    }
+    
+
+}
